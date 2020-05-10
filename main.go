38dddbd359a75ddc5598741a2180196da55d3632
Joe Garcia <joe@joe-garcia.com>
2020-05-10 16:56:03 -0400

    Moved GetSecret() to main.go

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"syscall"
 
 	"github.com/infamousjoeg/conceal/pkg/conceal"
 	"github.com/infamousjoeg/conceal/pkg/conceal/keychain"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
@@ -18,19 +21,29 @@ func main() {
 
 	flag.Parse()
 
-	// If an account is not given and version is not requested, error
-	if flag.Arg(0) == "" && *version == false && *list == false {
+	// If an account is given but not set using the -a flag, set account pointer to it
+	if *account == "" {
+		*account = flag.Arg(0)
+	}
+
+	// If an account is not given and version or list is not requested, show help
+	if *account == "" && !*version && !*list && !*remove {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	// If an account is given but not set using the -a flag, continue
-	if flag.Arg(0) != "" && *account == "" {
-		*account = flag.Arg(0)
+	// If remove is requested, remove secret from keychain
+	if *account != "" && *remove {
+		keychain.DeleteSecret(*account)
+		os.Exit(1)
+	}
+	if flag.Arg(0) != "" && flag.Arg(1) == "-r" {
+		keychain.DeleteSecret(flag.Arg(0))
+		os.Exit(1)
 	}
 
 	// If list is requested, return list of accounts with `Summon` label
-	if *list == true {
+	if *list {
 		accounts := keychain.ListSecrets()
 		fmt.Println("The following Summon accounts are in keychain:")
 		for account := range accounts {
@@ -40,17 +53,19 @@ func main() {
 	}
 
 	// If version is requested, give it
-	if *version == true {
+	if *version {
 		fmt.Printf("conceal v%s\n", conceal.FullVersionName)
 		os.Exit(1)
 	}
 
-	// If remove is requested, remove secret from keychain
-	if *account != "" && *remove == true {
-		keychain.DeleteSecret(*account)
-		os.Exit(1)
+	// Get secret value from STDIN
+	fmt.Println("Please enter the secret value: ")
+	byteSecretVal, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		log.Fatalln("An error occurred trying to read password from " +
+			"Stdin. Exiting...")
 	}
 
 	// Add secret and secret value to keychain
-	keychain.AddSecret(*account)
+	keychain.AddSecret(*account, byteSecretVal)
 }
