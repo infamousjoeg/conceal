c18e3ac4a249b86a17e33f7ef4b1012353a71a5e
Joe Garcia <infamousjoeg@users.noreply.github.com>
2025-06-25 09:21:02 -0400

    fix darwin keychain alias

--- pkg/conceal/keychain/keychain_darwin.go
+++ pkg/conceal/keychain/keychain_darwin.go
@@ -7,7 +7,7 @@ import (
 	"log"
 
 	"github.com/infamousjoeg/conceal/pkg/conceal/clipboard"
-	"github.com/keybase/go-keychain"
+	gokeychain "github.com/keybase/go-keychain"
 )
 
 // SecretExists is a boolean function to verify a secret is present in keychain
@@ -27,13 +27,13 @@ func SecretExists(secretID string) bool {
 // ListSecrets is a string array function that returns all secrets in keychain
 // with the label `summon`.
 func ListSecrets() []string {
-	query := keychain.NewItem()
-	query.SetSecClass(keychain.SecClassGenericPassword)
+	query := gokeychain.NewItem()
+	query.SetSecClass(gokeychain.SecClassGenericPassword)
 	query.SetService("summon")
-	query.SetMatchLimit(keychain.MatchLimitAll)
+	query.SetMatchLimit(gokeychain.MatchLimitAll)
 	query.SetReturnAttributes(true)
 	// Note: OSX use the term "account" to refer to the secret id.
-	secretIDs, err := keychain.QueryItem(query)
+	secretIDs, err := gokeychain.QueryItem(query)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -48,20 +48,20 @@ func ListSecrets() []string {
 // keychain.
 func AddSecret(secretID string, secret []byte) error {
 	// Create a new keychain item
-	item := keychain.NewItem()
-	item.SetSecClass(keychain.SecClassGenericPassword)
+	item := gokeychain.NewItem()
+	item.SetSecClass(gokeychain.SecClassGenericPassword)
 	item.SetService("summon")
 	item.SetAccount(secretID)
 	item.SetLabel("summon")
 	item.SetData(secret)
-	item.SetSynchronizable(keychain.SynchronizableNo)
-	item.SetAccessible(keychain.AccessibleAfterFirstUnlock)
+	item.SetSynchronizable(gokeychain.SynchronizableNo)
+	item.SetAccessible(gokeychain.AccessibleAfterFirstUnlock)
 
 	// Add new password item to keychain
-	err := keychain.AddItem(item)
+	err := gokeychain.AddItem(item)
 
 	// Duplicate item error
-	if err == keychain.ErrorDuplicateItem {
+	if err == gokeychain.ErrorDuplicateItem {
 		return fmt.Errorf("Secret %s already exists in keychain. Please use `conceal update` instead.", secretID)
 	}
 
@@ -80,12 +80,12 @@ func AddSecret(secretID string, secret []byte) error {
 
 // DeleteSecret is a boolean function that removes a secret from keychain.
 func DeleteSecret(secretID string) error {
-	item := keychain.NewItem()
-	item.SetSecClass(keychain.SecClassGenericPassword)
+	item := gokeychain.NewItem()
+	item.SetSecClass(gokeychain.SecClassGenericPassword)
 	item.SetService("summon")
 	item.SetAccount(secretID)
 
-	err := keychain.DeleteItem(item)
+	err := gokeychain.DeleteItem(item)
 	if err != nil {
 		return fmt.Errorf("An error occurred trying to remove secret from keychain. Secret '%s' not found in keychain.", secretID)
 	}
@@ -97,13 +97,13 @@ func DeleteSecret(secretID string) error {
 // adds it to the host clipboard for 15 seconds.
 func GetSecret(secretID string, delivery string) error {
 	// Build query for secret retrieval from Keychain
-	query := keychain.NewItem()
-	query.SetSecClass(keychain.SecClassGenericPassword)
+	query := gokeychain.NewItem()
+	query.SetSecClass(gokeychain.SecClassGenericPassword)
 	query.SetService("summon")
 	query.SetAccount(secretID)
-	query.SetMatchLimit(keychain.MatchLimitOne)
+	query.SetMatchLimit(gokeychain.MatchLimitOne)
 	query.SetReturnData(true)
-	results, err := keychain.QueryItem(query)
+	results, err := gokeychain.QueryItem(query)
 	if err != nil {
 		// Error occurred
 		return fmt.Errorf("An error occurred trying to get secret from keychain.")
@@ -126,13 +126,13 @@ func GetSecret(secretID string, delivery string) error {
 // UpdateSecret is a non-return function that updates the secret value in keychain.
 func UpdateSecret(secretID string, secret []byte) error {
 	// Build query for secret retrieval from Keychain
-	query := keychain.NewItem()
-	query.SetSecClass(keychain.SecClassGenericPassword)
+	query := gokeychain.NewItem()
+	query.SetSecClass(gokeychain.SecClassGenericPassword)
 	query.SetService("summon")
 	query.SetAccount(secretID)
-	query.SetMatchLimit(keychain.MatchLimitOne)
+	query.SetMatchLimit(gokeychain.MatchLimitOne)
 	query.SetReturnData(true)
-	results, err := keychain.QueryItem(query)
+	results, err := gokeychain.QueryItem(query)
 	if err != nil {
 		// Error occurred
 		return fmt.Errorf("An error occurred trying to get secret from keychain.")
@@ -140,17 +140,17 @@ func UpdateSecret(secretID string, secret []byte) error {
 		return fmt.Errorf("The secret %s does not exist in the keychain. Please use `conceal set` instead.", secretID)
 	} else {
 		// Create a new keychain item
-		item := keychain.NewItem()
-		item.SetSecClass(keychain.SecClassGenericPassword)
+		item := gokeychain.NewItem()
+		item.SetSecClass(gokeychain.SecClassGenericPassword)
 		item.SetService("summon")
 		item.SetAccount(secretID)
 		item.SetLabel("summon")
 		item.SetData(secret)
-		item.SetSynchronizable(keychain.SynchronizableNo)
-		item.SetAccessible(keychain.AccessibleAfterFirstUnlock)
+		item.SetSynchronizable(gokeychain.SynchronizableNo)
+		item.SetAccessible(gokeychain.AccessibleAfterFirstUnlock)
 
 		// Update password item in keychain
-		err := keychain.UpdateItem(query, item)
+		err := gokeychain.UpdateItem(query, item)
 		if err != nil {
 			return fmt.Errorf("An unexpected error occurred trying to update secret %s in the keychain.", secretID)
 		}
