d1df9badba57ed6113d3ac09e4655ffcdf61c547
Joe Garcia <infamousjoeg@users.noreply.github.com>
2025-06-27 19:35:02 -0400

    Improve Linux credential store errors

--- pkg/conceal/keychain/keychain_linux.go
+++ pkg/conceal/keychain/keychain_linux.go
@@ -66,14 +66,14 @@ func ListSecrets() []string {
 func AddSecret(secretID string, secret []byte) error {
 	_, col, session, err := getCollection()
 	if err != nil {
-		return fmt.Errorf("unexpected error adding secret %s to credential store", secretID)
+		return fmt.Errorf("unable to access credential store: %w", err)
 	}
 	if SecretExists(secretID) {
 		return fmt.Errorf("secret %s already exists in credential store; use `conceal update`", secretID)
 	}
 	sec := libsecret.NewSecret(session, nil, secret, "text/plain")
 	if _, err := col.CreateItem(secretID, sec, false); err != nil {
-		return fmt.Errorf("unexpected error adding secret %s to credential store", secretID)
+		return fmt.Errorf("unexpected error adding secret %s to credential store: %w", secretID, err)
 	}
 	return nil
 }
@@ -82,14 +82,14 @@ func AddSecret(secretID string, secret []byte) error {
 func DeleteSecret(secretID string) error {
 	_, col, _, err := getCollection()
 	if err != nil {
-		return fmt.Errorf("secret '%s' not found in credential store", secretID)
+		return fmt.Errorf("unable to access credential store: %w", err)
 	}
 	items, err := col.SearchItems(secretID)
 	if err != nil || len(items) == 0 {
 		return fmt.Errorf("secret '%s' not found in credential store", secretID)
 	}
 	if err := items[0].Delete(); err != nil {
-		return fmt.Errorf("secret '%s' not found in credential store", secretID)
+		return fmt.Errorf("secret '%s' not found in credential store: %w", secretID, err)
 	}
 	return nil
 }
@@ -98,7 +98,7 @@ func DeleteSecret(secretID string) error {
 func GetSecret(secretID string, delivery string) error {
 	_, col, session, err := getCollection()
 	if err != nil {
-		return fmt.Errorf("secret '%s' not found in credential store", secretID)
+		return fmt.Errorf("unable to access credential store: %w", err)
 	}
 	items, err := col.SearchItems(secretID)
 	if err != nil || len(items) == 0 {
@@ -106,7 +106,7 @@ func GetSecret(secretID string, delivery string) error {
 	}
 	sec, err := items[0].GetSecret(session)
 	if err != nil {
-		return fmt.Errorf("secret '%s' not found in credential store", secretID)
+		return fmt.Errorf("secret '%s' not found in credential store: %w", secretID, err)
 	}
 	password := string(sec.Value)
 	switch delivery {
@@ -122,7 +122,7 @@ func GetSecret(secretID string, delivery string) error {
 func UpdateSecret(secretID string, secret []byte) error {
 	_, col, session, err := getCollection()
 	if err != nil {
-		return fmt.Errorf("unexpected error updating secret %s in credential store", secretID)
+		return fmt.Errorf("unable to access credential store: %w", err)
 	}
 	items, err := col.SearchItems(secretID)
 	if err != nil || len(items) == 0 {
@@ -130,7 +130,7 @@ func UpdateSecret(secretID string, secret []byte) error {
 	}
 	sec := libsecret.NewSecret(session, nil, secret, "text/plain")
 	if _, err := col.CreateItem(secretID, sec, true); err != nil {
-		return fmt.Errorf("unexpected error updating secret %s in credential store", secretID)
+		return fmt.Errorf("unexpected error updating secret %s in credential store: %w", secretID, err)
 	}
 	return nil
 }
